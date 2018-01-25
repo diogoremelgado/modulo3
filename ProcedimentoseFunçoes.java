833958a2f136a95a20393a33fa6015d506777692
diogoremelgado <32169065+diogoremelgado@users.noreply.github.com>
2018-01-25 16:02:18 +0000

    Add files via upload

---
+++ ProcedimentoseFunçoes.java
@@ -0,0 +1,25 @@
+package bruxinhas;
+import java.util.Scanner;
+public class ProcedimentoseFunçoes {
+static int Maiorde3(int x, int y, int z)
+{if (x<y)
+	x=y;
+if (x<z)
+	x=z;
+return x ; }
+
+
+	public static void main(String[] args) throws Exception
+		{int x, y, z;
+		Scanner LerS = new Scanner (System.in);
+		System.out.print("Digite o 1º valor (x)");
+		x=LerS.nextInt();
+		System.out.print("Digite o 2º valor (y)");
+		y=LerS.nextInt();
+		System.out.print("Digite o 3º valor (z)");
+		z=LerS.nextInt();
+		System.out.print("Maior = " +Maiorde3 (x,y,z));
+		}
+}
+
+
